3442a41d3965c4053259062f92611748197ba1eb
reotsugua <renanaugusto.ras@gmail.com>
2023-03-21 23:09:16 -0300

    Primeito Commit

---
+++ projeto-web/projeto-web/src/main/java/com/example/projetoweb/ProjetoWebApplication.java
@@ -0,0 +1,14 @@
+package com.example.projetoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ projeto-web/projeto-web/src/test/java/com/example/projetoweb/ProjetoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
