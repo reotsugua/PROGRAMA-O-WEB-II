e3876505db3b682e72185c5660649fde64154fc2
reotsugua <renanaugusto.ras@gmail.com>
2023-03-23 17:01:46 -0300

    CRUD e testando usando POstman

--- projeto-web/projeto-web/src/main/java/com/example/projetoweb/controller/CategoriaController.java
+++ projeto-web/projeto-web/src/main/java/com/example/projetoweb/controller/CategoriaController.java
@@ -1,9 +1,27 @@
 package com.example.projetoweb.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.projetoweb.model.Categoria;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
+
+    private List<Categoria> lista = new ArrayList<>();
+    private Integer contador = 1;
+    @GetMapping
+    public List<Categoria> listar(){
+        return lista;
+    }
+
+    @PostMapping
+    public Categoria criar(@RequestBody Categoria categoria){
+        categoria.setId(contador);
+        lista.add(categoria);
+        contador++;
+        return categoria;
+    }
 }
--- projeto-web/projeto-web/src/main/java/com/example/projetoweb/model/Categoria.java
+++ projeto-web/projeto-web/src/main/java/com/example/projetoweb/model/Categoria.java
@@ -1,4 +1,15 @@
 package com.example.projetoweb.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@RequiredArgsConstructor pra usar o NonNull
 public class Categoria {
+    private Integer id;
+    //@NonNull
+    private String nome;
+    private String descricao;
 }
