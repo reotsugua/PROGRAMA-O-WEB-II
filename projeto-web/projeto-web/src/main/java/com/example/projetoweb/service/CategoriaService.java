a23adf007c74dc4662487bef27f9b67a52ccfd59
reotsugua <renanaugusto.ras@gmail.com>
2023-03-24 22:03:16 -0300

    Apredento Service

--- projeto-web/projeto-web/src/main/java/com/example/projetoweb/service/CategoriaService.java
+++ projeto-web/projeto-web/src/main/java/com/example/projetoweb/service/CategoriaService.java
@@ -1,4 +1,12 @@
 package com.example.projetoweb.service;
 
+import com.example.projetoweb.model.Categoria;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CategoriaService {
+
+    public Categoria pegarCategoriaById(){
+        return null;
+    }
 }
